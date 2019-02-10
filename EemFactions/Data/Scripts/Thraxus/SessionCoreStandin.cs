85e758270b399d9d7226c49d40f49124e30fa1ef
Thraxus <mornadak@hotmail.com>
2019-02-10 15:03:16 -0500

    Add project files.

---
+++ EemFactions/Data/Scripts/Thraxus/SessionCoreStandin.cs
@@ -0,0 +1,29 @@
+﻿using VRage.Game.Components;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+namespace EemFactions
+{
+	[MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+	public class SessionCoreStandin : MySessionComponentBase
+	{
+		private bool _initialized;
+
+		/// <summary>
+		/// Runs every tick before the simulation is updated
+		/// </summary>
+		public override void UpdateBeforeSimulation()
+		{
+			if (!_initialized) Initialize();
+		}
+
+		private void Initialize()
+		{
+			_initialized = true;
+		}
+
+		protected override void UnloadData()
+		{
+
+		}
+	}
+}
